304628459f4f0bfe147b904494f29e3f885419ca
Mirozell <mirozell@gmail.com>
2026-06-07 12:09:09 -0500

    launch shortcuts

---
+++ Doomsday/launch-mirofarm.bat
@@ -0,0 +1,4 @@
+set doomsday="C:\Program Files (x86)\Steam\steamapps\common\Doomsday Last Survivors\doomsday.exe"
+
+runas /user:mirofarm /savecred %doomsday%
+
---
+++ Doomsday/launch-miromini.bat
@@ -0,0 +1,3 @@
+set doomsday="C:\Program Files (x86)\Steam\steamapps\common\Doomsday Last Survivors\doomsday.exe"
+
+runas /user:miromini /savecred %doomsday%
